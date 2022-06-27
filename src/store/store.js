8e1a9dd8fc33f1c775edccbcd2dc1bbbf689b835
Kevin Machava <kevinmachava20@gmail.com>
2022-06-28 00:25:13 +0200

    Add files via upload

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { firebaseAuth, firebaseDb } from 'boot/firebase'
 
 const state = {
-
+	userDetails: {}
 }
 const mutations = {
-
+	setUserDetails(state, payload) {
+		state.userDetails = payload
+	}
 }
 const actions = {
 	registerUser({}, payload) {
@@ -29,8 +31,51 @@ const actions = {
 			})
 			.catch(error => {
 				console.log(error.message)
-			})		
-	} 
+			})
+	},
+	logoutUser() {
+		firebaseAuth.signOut()
+	},
+	handleAuthStateChanged({ commit, dispatch, state }) {
+		firebaseAuth.onAuthStateChanged(user => {
+		  if (user) {
+		    // User is logged in.
+		    let userId = firebaseAuth.currentUser.uid
+		    firebaseDb.ref('users/' + userId).once('value', snapshot => {
+		    	let userDetails = snapshot.val()
+		    	commit('setUserDetails', {
+		    		name: userDetails.name,
+		    		email: userDetails.email,
+		    		userId: userId
+		    	})
+		    })
+		    dispatch('firebaseUpdateUser', {
+		    	userId: userId,
+		    	updates: {
+		    		online: true
+		    	}
+		    })
+		    this.$router.push('/')
+		  }
+		  else {
+		  	// User is logged out.
+		  	dispatch('firebaseUpdateUser', {
+		  		userId: state.userDetails.userId,
+		  		updates: {
+		  			online: false
+		  		}
+		  	})
+		  	commit('setUserDetails', {})
+		  	this.$router.replace('/auth')
+		  }
+		})
+	},
+
+	firebaseUpdateUser({}, payload) {
+		if (payload.userId) {
+			firebaseDb.ref('users/' + payload.userId).update(payload.updates)
+		}
+	}
 }
 const getters = {
 
